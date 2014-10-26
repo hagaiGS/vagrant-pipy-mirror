79fe567a3c6e9d141f31283cfed3019203c42086
hagaiGS <hagai@gigaspaces.com>
2014-10-26 08:52:14 +0200

    added logging

--- provision.sh
+++ provision.sh
@@ -7,7 +7,7 @@ T="$(date +%s)"
 
 if [ -f /.installed ]
 then
-	echo 'Already installed.'
+	echo '### Already installed.'
 	source /home/vagrant/env/bin/activate
 
 else
@@ -15,20 +15,26 @@ else
 	echo '------------'
 	cd /home/vagrant
 	sudo apt-get update -y
-	sudo apt-get install python-pip python-dev build-essential -y
+	echo "### Installing python-pip python-dev build-essential nginx"
+	sudo apt-get install python-pip python-dev build-essential nginx -y
 	sudo pip install --upgrade pip
 	sudo pip install virtualenv
 	sudo pip install --upgrade virtualenv
-	sudo apt-get install nginx -y
 	virtualenv env
 	source /home/vagrant/env/bin/activate
+	echo "### Installing devpi-server"
 	pip install -U devpi-server
+	echo "### Confuring nginx"
 	sudo mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.original # backup the original
 	sudo cp /vagrant/nginx.conf /etc/nginx/nginx.conf
 	sudo touch /.installed # mark for the next 'vagrant up' that everything is already installed
+	echo "### Starting nginx"
 	sudo nginx
 fi
+
+echo "### Starting devpi-server"
 devpi-server --port 4040 --start
+echo "### Reloading nginx"
 sudo nginx -s reload
 
 # Print how long the bootstrap script took to run
