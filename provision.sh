c8e3e3816850c5b251256ea02dabc51006ba072a
hagaiGS <hagai@gigaspaces.com>
2014-10-27 16:06:35 +0200

    using bandersnatch instead of devpi

--- provision.sh
+++ provision.sh
@@ -1,45 +1,38 @@
 # This script configures and starts a PyPi mirror and http-server for cloudify offline
 
-# Set start time so we know how long the bootstrap takes
-T="$(date +%s)"
  # TODO: fix the env location
  # TODO: check installing nginx without sudo permissions
  # TODO: use user ubuntu instead of vagrant
+set -e
 
-if [ -f /.installed ]
-then
-	echo '### Already installed.'
-	source /home/vagrant/env/bin/activate
+HOME_FOLDER="/home/vagrant"
+FILES_DIR=/vagrant
 
-else
-	echo ' INSTALLING '
-	echo '------------'
-	cd /home/vagrant
-	sudo apt-get update -y
-	echo "### Installing python-pip python-dev build-essential nginx"
-	sudo apt-get install python-pip python-dev build-essential nginx -y
-	sudo pip install --upgrade pip
-	sudo pip install virtualenv
-	sudo pip install --upgrade virtualenv
-	virtualenv env
-	source /home/vagrant/env/bin/activate
-	echo "### Installing devpi-server"
-	pip install -U devpi-server
-	echo "### Confuring nginx"
-	sudo mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.original # backup the original
-	sudo cp /vagrant/nginx.conf /etc/nginx/nginx.conf
-	sudo touch /.installed # mark for the next 'vagrant up' that everything is already installed
-	mkdir /home/vagrant/nginx_server
-	mkdir /home/vagrant/nginx_server/cloudify
-	echo "### Starting nginx"
-	sudo nginx
-fi
-
-echo "### Starting devpi-server"
-devpi-server --port 4040 --start
-echo "### Reloading nginx"
-sudo nginx -s reload
-
-# Print how long the bootstrap script took to run
-T="$(($(date +%s)-T))"
-echo "### Time bootstrap took: ${T} seconds ###"
+echo "Using $HOME_FOLDER as a home folder"
+echo "Using $FILES_DIR as a files folder"
+echo ' INSTALLING '
+echo '------------'
+cd "$HOME_FOLDER"
+sudo apt-get update -y
+echo "### Installing python-pip python-dev build-essential nginx"
+sudo apt-get install python-pip python-dev build-essential nginx -y
+sudo pip install --upgrade pip
+sudo pip install virtualenv
+sudo pip install --upgrade virtualenv
+virtualenv env
+source "$HOME_FOLDER/env/bin/activate"
+echo "### Installing bandersnatch"
+pip install -r https://bitbucket.org/pypa/bandersnatch/raw/stable/requirements.txt
+echo "### Confuring nginx"
+sudo mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.original # backup the original
+sudo cp "$FILES_DIR/nginx.conf" /etc/nginx/nginx.conf
+sudo cp "$FILES_DIR/bandersnatch.conf" /etc/nginx/sites-available/bandersnatch.conf
+sudo ln -s /etc/nginx/sites-available/bandersnatch.conf /etc/nginx/sites-enabled/bandersnatch.conf
+sudo mkdir /srv/cloudify
+echo "### Starting nginx"
+sudo nginx
+echo "Using nohup to download PyPi mirror"
+nohup sudo bandersnatch mirror &
+PID=$!
+echo "nohup pid is $PID"
+echo "execute ps -m $PID to check if the download has finished"
